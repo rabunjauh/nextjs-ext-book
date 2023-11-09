de12c977488317bf31afd2058860822e74c93246
Mustafa <mustafa.visionet@gmail.com>
2023-11-09 17:42:29 +0700

    modiy component DeleteGroup(add delete confirmation)

--- src/components/group/DeleteGroup.tsx
+++ src/components/group/DeleteGroup.tsx
@@ -1,10 +1,12 @@
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 import { Button } from "../ui/button";
 import { toast, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 export default function DeleteGroup({ table }) {
   const router = useRouter();
+  const [confirmModal, setConfirmModal] = useState(false);
 
   const executeDelete = async () => {
     try {
@@ -47,7 +49,35 @@ export default function DeleteGroup({ table }) {
   return (
     <div>
       <ToastContainer />
-      <Button variant="outline" onClick={executeDelete}>
+      {confirmModal ? (
+        <div className="fixed inset-0 flex justify-center items-center transition-colors visible bg-black/70 z-50">
+          <div className="bg-white p-5 rounded-lg w-96">
+            <h3 className="font-bold text-lg">Delete Item</h3>
+            <div>
+              Are you sure you want to delete this item?
+              <div className="flex justify-end mt-2">
+                <div className="px-3">
+                  <Button
+                    onClick={() => {
+                      setConfirmModal(false);
+                      window.location.reload();
+                    }}
+                  >
+                    No
+                  </Button>
+                </div>
+
+                <div>
+                  <Button variant="destructive" onClick={executeDelete}>
+                    Yes
+                  </Button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : null}
+      <Button variant="outline" onClick={() => setConfirmModal(true)}>
         Delete Selected
       </Button>
     </div>
